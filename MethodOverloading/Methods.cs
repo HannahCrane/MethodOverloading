4fcace0e566a0694337866e771ef22f54be57d2c
HannahCrane <84360098+HannahCrane@users.noreply.github.com>
2023-02-02 19:28:26 -0500

    initial commit

--- MethodOverloading/Methods.cs
+++ MethodOverloading/Methods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,37 @@ namespace MethodOverloading
 {
     internal class Methods
     {
+      //Create a method named Add, that accepts two integers and returns the sum of those two integers
+      public static int Add(int x, int y)
+        { 
+            return x + y;
+        }
+
+        //Now create an overload of the Add method to account for being able to add two decimals together
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        //Now create another overload of the Add method that returns a string and accepts
+        //3 parameters: 2 integers and 1 boolean
+        //If the boolean parameter is equal to true, the Add method will return the sum of the 2 numbers
+        //plus the word “dollars” at the end of the string.
+        //You must also account for the appropriate grammar of the string - so it can’t be “1 dollars”
+        public static string Add(int x, int y, bool withDollars)
+        {
+            if (withDollars && (x + y) != 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if (withDollars && (x + y) == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else
+            {
+                return $"{x + y}";
+            }
+        }
     }
 }
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MethodOverloading;
+
+
+
+var answer = Methods.Add(2, 2, true);
+Console.WriteLine(answer);
+
+
